3bf211c2bc43e3dfe559beeee0186acc9999da96
Luxx Labs <13350855+luxxyarns@users.noreply.github.com>
2021-08-12 10:55:19 +0200

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -6,22 +6,4 @@ import PackageDescription
 
 let package = Package(
     name: "JSQWebViewController"
-)
-
-//
-//  Created by Jesse Squires
-//  http://www.jessesquires.com
-//
-//
-//  Documentation
-//  http://www.jessesquires.com/JSQWebViewController
-//
-//
-//  GitHub
-//  https://github.com/jessesquires/JSQWebViewController
-//
-//
-//  License
-//  Copyright © 2015 Jesse Squires
-//  Released under an MIT license: http://opensource.org/licenses/MIT
-//
+) 
